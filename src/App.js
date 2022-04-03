6c283543bafc475b8511ebf6fc198478064dfc02
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2022-04-03 10:50:13 +0530

    refactor:navbar placced outside routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import {Routes, Route, } from "react-router-dom";
-import {LandingRoute} from './routes/landingRoute'
+import { Routes, Route } from "react-router-dom";
+import { LandingRoute } from "./routes/landingRoute";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <>  
+    <>
+      <Navbar />
       <Routes>
-      <Route path="/" element={<LandingRoute/>}/>
+        <Route path="/" element={<LandingRoute />} />
       </Routes>
     </>
   );
